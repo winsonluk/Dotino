2cc8327fc9d0d3556bc49c347248a686985797be
Winson <winson.luk@gmail.com>
2017-03-28 15:49:52 -0700

    add comments, optimize for mobile

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
+// Initialize List for list.js
 var options = {
     valueNames: [ 'city', 'state' ],
-    page: 10
+    page: 7
 };
-
 var userList = new List('wrapper', options);
 
+// Show error message if no city found, else update list
 function process() {
     if (userList.visibleItems.length == 0) {
         $( "#error_message" ).fadeTo(2000, 1);
     }
+    $('table.table-fill tr').each(function(n, row) {
+        var $row = $(row);
+        var domRow = $(row)[0];
+        var city = "window.document.location='city.html?city=" + $row.find('td:eq(0)').text().replace(/\s+/g, '') + "';";
+        row.setAttribute("class", "clickable-row");
+        row.setAttribute("onclick", city);
+    });
 }
 
+// Scroll in list of cities
 window.smoothScroll = function(target) {
     document.getElementById("user_input").focus();
     reddit.style.display = 'none';
     yelp.style.display = 'none';
     var scrollContainer = target;
-    do { //find scroll container
+    do { // find scroll container
         scrollContainer = scrollContainer.parentNode;
         if (!scrollContainer) return;
         scrollContainer.scrollTop += 1;
     } while (scrollContainer.scrollTop == 0);
 
     var targetY = 0;
-    do { //find the top of target relatively to the container
+    do { // find the top of target relatively to the container
         if (target == scrollContainer) break;
         targetY += target.offsetTop;
     } while (target = target.offsetParent);
@@ -35,10 +44,10 @@ window.smoothScroll = function(target) {
         c.scrollTop = a + (b - a) / 30 * i;
         setTimeout(function(){ scroll(c, a, b, i); }, 20);
     }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0); // start scrolling
 }
 
+// Fade-in logo and animate search bar movement to top
 $(document).ready(function() {
     $("input").on("click", function(e) {
         e.preventDefault();
@@ -47,38 +56,41 @@ $(document).ready(function() {
             top: '0%',
             marginBottom: - $div.height()
         }, 1000 );
-        $( "#logowide" ).fadeTo(2000, 1);
+        $( "#logo_wide" ).fadeTo(2000, 1);
     });
 });
 
+// Scroll to top before reload
 var toScroll = true;
 window.onbeforeunload = function() {
     toScroll = false;
     $(window).scrollTop(0);
 }
 
-var reached = true;
-if (reached) {
-$(document).on('scroll', function() {
-  if ($(this).scrollTop() >= 100 && reached) {
-      reached = false;
-      $("#user_input").click();
-  }
-});
+// Scroll to list page when threshold reached
+var reached = false;
+if (!reached) {
+    $(document).on('scroll', function() {
+      if ($(this).scrollTop() >= 100 && !reached) {
+          reached = true;
+          $("#user_input").click();
+      }
+    });
 }
 
+// Prevent scrolling on list page
 $(function() {
     var scrollPoint = $(window).height();
     var scrolledPast = false;
     $(window).scroll(function() {
         if (toScroll) {
-        $(window).scrollTop() > scrollPoint ? scrolledPast = true : '';
-        $(window).scrollTop() < scrollPoint && scrolledPast == true ? $(window).scrollTop(scrollPoint) : '';
-        $(window).scrollTop() > scrollPoint && scrolledPast == true ? $(window).scrollTop(scrollPoint) : '';
+            $(window).scrollTop() > scrollPoint ? scrolledPast = true : '';
+            scrolledPast ? $(window).scrollTop(scrollPoint) : '';
         }
     }).scroll();
 });
 
+// Link to corresponding city page for cities in list
 $('table.table-fill tr').each(function(n, row) {
     var $row = $(row);
     var domRow = $(row)[0];
@@ -87,17 +99,14 @@ $('table.table-fill tr').each(function(n, row) {
     row.setAttribute("onclick", city);
 });
 
-$( "#user_input " ).keypress(function() {
-$('table.table-fill tr').each(function(n, row) {
-    var $row = $(row);
-    var domRow = $(row)[0];
-    var city = "window.document.location='city.html?city=" + $row.find('td:eq(0)').text().replace(/\s+/g, '') + "';";
-    row.setAttribute("class", "clickable-row");
-    row.setAttribute("onclick", city);
+// Refresh city page as search updates
+$( "#user_input" ).keypress(function() {
+    $('table.table-fill tr').each(function(n, row) {
+        var $row = $(row);
+        var domRow = $(row)[0];
+        var city = "window.document.location='city.html?city=" + $row.find('td:eq(0)').text().replace(/\s+/g, '') + "';";
+        row.setAttribute("class", "clickable-row");
+        row.setAttribute("onclick", city);
+    });
+    $( "#error_message" )[0].style.opacity = '0';
 });
-});
-
-/*
-$row.find('th:last').after('<th>Enquire</th>');
-$row.find('td:last').after('<td><a class="mailenquiry" href="mailto:foo@foo.co.uk?subject=Enquiry about ' + module + ' on ' + date + '">Enquire</a></td>');
-*/
