fc5a0e45a4de23e2abe861d817aa797fbc5e711b
Winson <winson.luk@gmail.com>
2017-03-30 20:51:42 -0700

    add firebase, reddit parent

--- js/cityscript.js
+++ js/cityscript.js
@@ -34,39 +34,7 @@ $(document).ready(function() {
                 }
                 for (k = 0; k < 3; k++) {
                     tempText = document.createElement('div');
-                    switch (k) {
-                        case 0:
-                            tempText.className = 'text-top';
-                            tempText.innerHTML = 'SCORE:\<br\>' + csv[i]['score'];
-                            break;
-                        case 1:
-                            tempText.className = 'text-middle';
-                            var comment = removeLink(csv[i]['comment']);
-                            var totalChars = 300;
-                            var halfNumChars = totalChars / 2;
-                            var yelpIndex = comment.indexOf("yelp.com/biz/");
-                            var lower = yelpIndex - halfNumChars;
-                            var upper = yelpIndex + halfNumChars;
-                            if (upper > comment.length) {
-                                lower = lower - (upper - comment.length);
-                                upper = comment.length;
-                            } else if (lower < 0) {
-                                upper = upper + Math.min(comment.length - upper, Math.abs(lower));
-                            }
-                            if (lower < 0) {
-                                comment = "..." + comment.substring(3);
-                            }
-                            if (upper == totalChars) {
-                                comment = comment.substring(0, upper - 3) + "...";
-                            }
-                            lower = Math.max(0, lower);
-                            tempText.innerHTML = comment.substring(lower, upper);
-                            break;
-                        case 2:
-                            tempText.className = 'text-bottom';
-                            tempText.innerHTML = 'POSTED:\<br\>' + timeConverter(csv[i]['time']);
-                            break;
-                    }
+                    createDot(csv, tempText, j, k);
                     tempDot.appendChild(tempText);
                 }
                 tempContainer.appendChild(tempDot);
@@ -91,4 +59,85 @@ function timeConverter(UNIX_timestamp) {
 
 function removeLink(link) {
     return link.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').replace(/\[|\]|\(|\)/g, ' ');
+}
+
+function createDot(csv, tempText, j, k) {
+    switch (j) {
+        case 0:
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var title = (JSON.parse(this.responseText))['data']['children'][0]['data']['title'];
+                    var url = (JSON.parse(this.responseText))['data']['children'][0]['data']['url'];
+                    var num_comments = (JSON.parse(this.responseText))['data']['children'][0]['data']['num_comments'];
+                    switch (k) {
+                        case 0:
+                            tempText.className = 'text-top';
+                            tempText.innerHTML = 'REPLIES:\<br\>' + num_comments;
+                            break;
+                        case 1:
+                            tempText.className = 'text-middle';
+                            tempText.innerHTML = title;
+                            break;
+                        case 2:
+                            tempText.className = 'text-bottom';
+                            tempText.innerHTML = '/r/' + location.search.split('?')[1];
+                            break;
+                  }
+                }
+            };
+            xhttp.open("GET", "https://api.reddit.com/by_id/t3_" + csv[i]['parent'], true);
+            xhttp.send();
+            break;
+        case 1:
+            switch (k) {
+                case 0:
+                    tempText.className = 'text-top';
+                    tempText.innerHTML = 'SCORE:\<br\>' + csv[i]['score'];
+                    break;
+                case 1:
+                    tempText.className = 'text-middle';
+                    var comment = csv[i]['comment'];
+                    var totalChars = 300;
+                    var halfNumChars = totalChars / 2;
+                    if (comment.length > totalChars) {
+                        var yelpIndex = comment.indexOf("yelp.com/biz/");
+                        var index = halfNumChars;
+                        while (index < yelpIndex && index + halfNumChars < comment.length) {
+                            index++;
+                        }
+                        var lower = index - halfNumChars;
+                        var upper = index + halfNumChars;
+                        if (lower != 0) {
+                            before = comment.substring(0, lower);
+                            after = comment.substring(lower);
+                            after = "..." + after.substring(3);
+                            comment = before + after;
+                        }
+                        if (upper < comment.length) {
+                            comment = comment.substring(0, upper - 3) + "...";
+                        }
+                        tempText.innerHTML = removeLink(comment.substring(lower, upper));
+                    } else {
+                        tempText.innerHTML = removeLink(comment);
+                    }
+                    break;
+                case 2:
+                    tempText.className = 'text-bottom';
+                    tempText.innerHTML = 'POSTED:\<br\>' + timeConverter(csv[i]['time']);
+                    break;
+            }
+            break;
+        case 2:
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+                }
+            };
+            xhttp.open("GET", "https://api.yelp.com/v3/businesses/thin-and-craft-san-diego", true);
+            xhttp.setRequestHeader("Authorization", "Bearer V1EiegiHur2A14Qa9ll54v_I5_iS5SA3hSsIczZKBFj_GEV1kg4sJhAWb9FmeEa69vppLLFPpeio2mDmrVMxjA9-K8z6J1cs4thVJA05Vsq8U7I4CubaafvI3KjdWHYx");
+            xhttp.send();
+            break;
+    }
 }
