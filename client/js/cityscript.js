e48a40611d7f3f62bda9604e8499eadce5a16782
Winson <winson.luk@gmail.com>
2017-03-31 00:30:09 -0700

    add server

--- client/js/cityscript.js
+++ client/js/cityscript.js
@@ -133,9 +133,8 @@ function createDot(csv, tempText, j, k) {
             xhttp.onreadystatechange = function() {
                     console.log(this.responseText);
             };
-            xhttp.open("GET", "https://api.yelp.com/v3/businesses/thin-and-craft-san-diego", true);
-            xhttp.setRequestHeader("Authorization", "Bearer V1EiegiHur2A14Qa9ll54v_I5_iS5SA3hSsIczZKBFj_GEV1kg4sJhAWb9FmeEa69vppLLFPpeio2mDmrVMxjA9-K8z6J1cs4thVJA05Vsq8U7I4CubaafvI3KjdWHYx");
-            xhttp.send(null);
+            xhttp.open("GET", "https://crossorigin.me/https://api.yelp.com/v3/businesses/thin-and-craft-san-diego", true);
+            xhttp.send();
             break;
     }
 }
