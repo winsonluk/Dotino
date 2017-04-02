f6fd27366f2dfc126c57a5f2a449a18d1fb807bd
Winson <winson.luk@gmail.com>
2017-04-02 01:34:36 -0700

    update

--- server/main.py
+++ server/main.py
@@ -3,20 +3,23 @@ import json
 
 
 from flask import Flask, request
-from flask.ext.cors import CORS, cross-origin
 
 
 import yelp
 
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+# allow cross-domain uploads (CORS)
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'https://dotino.com')
+    return response
 
 
 # GET from https://api.dotino.com/yelp?business=RESTAURANT_ID_HERE to return Yelp restaurant info
 @app.route('/yelp')
-@cross_origin()
 def get_business():
     business = request.args.get('business')
     bearer_token = yelp.obtain_bearer_token(yelp.API_HOST, yelp.TOKEN_PATH)
@@ -24,9 +27,8 @@ def get_business():
 
 
 @app.route('/')
-@cross_origin()
 def home():
     return 'For usage information regarding the Dotino API, please visit https://github.com/winsonluk/Dotino/blob/master/README.md'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
